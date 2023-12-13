4c04b391f8697d5fc76d1e2d419bbb10e8825e05
Javier Jiménez-Alfaro Hacha <javilin7967@gmail.com>
2023-12-13 16:39:34 +0100

    pantalla inicio estilizada

--- app/src/main/java/com/hachatml/blackjack/screens/MainMenu.kt
+++ app/src/main/java/com/hachatml/blackjack/screens/MainMenu.kt
@@ -1,34 +1,58 @@
 package com.hachatml.blackjack.screens
 
 import android.graphics.Color
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-    @Composable
-    fun TitleAndButtons(navController: NavHostController){
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxSize().padding(10.dp)
+import com.hachatml.blackjack.R
+
+@Composable
+fun TitleAndButtons(navController: NavHostController) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painterResource(id = R.drawable.casino),
+                contentScale = ContentScale.Crop
+            )
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Logo del BlackJack",
+            modifier = Modifier.size(300.dp).padding(50.dp)
+        )
+        Button(
+            onClick = { navController.navigate(Routes.ModoPVP.route) },
+            shape = RoundedCornerShape(10),
+            colors = ButtonDefaults.buttonColors(
+                androidx.compose.ui.graphics.Color.White
+            ),
+            modifier = Modifier.padding(5.dp)
         ) {
-            Text(text = "BLACKJACK", fontSize = 50.sp,modifier = Modifier.padding(20.dp))
-            Button(onClick = { navController.navigate(Routes.ModoPVP.route) },modifier = Modifier.padding(20.dp)) {
-                Text(text = "Jugar")
-            }
+            Text(text = "Jugar")
         }
     }
+}
--- app/src/main/java/com/hachatml/blackjack/screens/ModoJugadorVsJugador.kt
+++ app/src/main/java/com/hachatml/blackjack/screens/ModoJugadorVsJugador.kt
@@ -1,5 +1,6 @@
 package com.hachatml.blackjack.screens
 
+import android.graphics.drawable.shapes.Shape
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -7,11 +8,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.ButtonColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,12 +22,16 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Shapes
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.navigation.NavController
 import com.hachatml.blackjack.R
@@ -38,9 +45,12 @@ import com.hachatml.blackjack.R
         Column(
             verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxSize().paint(
-                painterResource(id = R.drawable.fondo),
-                contentScale = ContentScale.Crop)
+            modifier = Modifier
+                .fillMaxSize()
+                .paint(
+                    painterResource(id = R.drawable.fondo),
+                    contentScale = ContentScale.Crop
+                )
         ) {
             PintarJugador1(cycle,VM)
             Botonera(cycle, navController,VM)
@@ -50,7 +60,7 @@ import com.hachatml.blackjack.R
 
     @Composable
     fun PintarJugador2(cycle: Boolean, VM: JvJViewModel) {
-        LazyRow(horizontalArrangement = Arrangement.spacedBy((-200).dp)) {
+        LazyRow(horizontalArrangement = Arrangement.spacedBy((-200).dp),modifier = Modifier.padding(20.dp)) {
             //todo hacer que se vea la carta de a quien le toque
             items(VM.Mjugador2.Mano) {
                 if (!(VM.Mjugador2.suTurno)) {
@@ -77,15 +87,15 @@ import com.hachatml.blackjack.R
             horizontalAlignment = Alignment.CenterHorizontally
         ){
             Row {
-                Button(onClick = { VM.dameCarta() },enabled = !(VM.partidaFinalizada)) {
+                Button(onClick = { VM.dameCarta() },enabled = !(VM.partidaFinalizada), shape = RoundedCornerShape(10), colors = ButtonDefaults.buttonColors(Color.White), modifier = Modifier.padding(5.dp)) {
                     Text(text = "Dame Carta")
                 }
-                Button(onClick = { VM.plantarse() },enabled = !(VM.partidaFinalizada)) {
+                Button(onClick = { VM.plantarse() },enabled = !(VM.partidaFinalizada), shape = RoundedCornerShape(10), colors = ButtonDefaults.buttonColors(Color.White), modifier = Modifier.padding(5.dp)) {
                     Text(text = "plantarse")
                 }
             }
             if (VM.partidaFinalizada) {
-                Button(onClick = { navController.navigate(Routes.PantallaVictoria.route) }) {
+                Button(onClick = { navController.navigate(Routes.PantallaVictoria.route) }, shape = RoundedCornerShape(10), colors = ButtonDefaults.buttonColors(Color.White), modifier = Modifier.padding(5.dp)) {
                     Text(text = "Ver ganador")
                 }
             }
@@ -95,7 +105,7 @@ import com.hachatml.blackjack.R
     @Composable
     fun PintarJugador1(cycle: Boolean,VM: JvJViewModel) {
         //todo hacer que se muestren cartas solo en el turno de uno
-        LazyRow(horizontalArrangement = Arrangement.spacedBy((-200).dp)) {
+        LazyRow(horizontalArrangement = Arrangement.spacedBy((-200).dp), modifier = Modifier.padding(20.dp)) {
             items(VM.Mjugador1.Mano) {
                 if (!(VM.Mjugador1.suTurno)) {
                     Image(
